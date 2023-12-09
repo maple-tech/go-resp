5b748feb1dc5f833c197fa0f0473e226bd844b24
Chris Pikul <chris@workmaple.com>
2023-12-09 13:44:54 +0300

    prep repository

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maple-tech/go-resp
+
+go 1.21.0
---
+++ lib.go
@@ -0,0 +1,4 @@
+// package resp provides encoders, decoders, and utilities for formatting and
+// reading RESP version 2 & 3 messages. This is the primary protocol used by
+// Redis for communication, but it's also a neat encoding format.
+package resp
